aee68d8b84b3236a521891439f737729fe65069e
beyon <bojan.aleksich@gmail.com>
2016-01-21 17:47:40 +0100

    Most difficult method I've ever worked on - DONE! :D

--- src/testGit/Metode.java
+++ src/testGit/Metode.java
@@ -2,4 +2,9 @@ package testGit;
 
 public class Metode {
 
+	/** Print Text Method, very hard to implement... :D */
+	public static void printText(String text) {
+		System.out.println(text);
+	}
+	
 }
